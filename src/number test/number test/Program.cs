68aca5c6bd970314bd107fa855393be4b498eb61
Ben Cope <ben.cope@inputsquared.co.uk>
2023-11-08 15:27:53 +0000

    Added comic book repository

---
+++ src/number test/number test/Program.cs
@@ -0,0 +1,13 @@
+﻿double average_a = 1.0;
+int average_b = 0;
+int count = 0;
+for (int i = 2; i < 100; i++)
+{
+    average_a += i;
+    average_b += i;
+    average_a /= 2.0;
+    count = i;
+}
+average_b /= count;
+
+Console.WriteLine($"{average_a} - {average_b}");
